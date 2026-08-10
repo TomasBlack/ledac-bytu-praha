b4894ecf6ae4402b136eddc89a36c6aca4837c42
TomasBlack <mrakoslapovo@gmail.com>
2026-08-10 17:15:01 +0200

    Update collector.py

--- scripts/collector.py
+++ scripts/collector.py
@@ -1,1 +1,19 @@
-print("Collector připraven")
+import json
+
+apartments = [
+    {
+        "id": 1,
+        "title": "Testovací byt",
+        "price": 8000000
+    }
+]
+
+with open("apartments-generated.json", "w", encoding="utf-8") as f:
+    json.dump(
+        apartments,
+        f,
+        ensure_ascii=False,
+        indent=2
+    )
+
+print("Soubor apartments-generated.json vytvořen")
